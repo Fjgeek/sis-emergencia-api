c3f9c2cf7a05b82b64e4f6826283a0548c4adfe7
JoseChirinos <josealexcba@gmail.com>
2019-07-04 01:43:48 -0400

    add demand service is work

--- models/emergency-model.php
+++ models/emergency-model.php
@@ -63,6 +63,64 @@
       );
     }
 
+    public function successEmergency($id_bed, $rfid){
+      var_dump($id_bed, $rfid);
+      // Verificamos si existe una peticion
+      $where = array(
+        "bed_id" => $id_bed,
+        "enabled"=> new FluentLiteral("1"),
+      );
+      $request_emergency = $this->fpdo->from($this->table)->where($where)->limit(1)->execute();
+      if($request_emergency->rowCount() != 0){ // Si existe una peticion
+
+        // Verificamos si existe la enfermera
+        $request_nurse = $this->fpdo->from('nurse')->where('rfid',$rfid)->limit(1)->execute();
+        if($request_nurse->rowCount() != 0 ){ // Si existe la enfermera
+          $nurse = $request_nurse->fetchObject();
+          $emergency = $request_emergency->fetchObject();
+          // Generamos una demanda
+          $values = array(
+            "nurse_id"=> $nurse->id_nurse,
+            "emergency_id"=> $emergency->id_emergency,
+            "time_attend"=> new FluentLiteral("CURRENT_TIME"),
+            "created"=> new FluentLiteral("CURRENT_TIMESTAMP"),
+          );
+          $insert_demand = $this->fpdo->insertInto('demand')->values($values);
+          $insert_id = $insert_demand->execute();
+          // Actualizamos la emergencia -> atendida
+          $set = array(
+            "enabled"=> new FluentLiteral("0")
+          );
+          $update_emergency = $this->fpdo->update($this->table)->set($set)->where('id_emergency', $emergency->id_emergency)->execute();
+
+          $result = array(
+            "id_demand" => $insert_id
+          );
+          $status = true;
+          $msg = "Emergencia Atendida";
+
+        }else{ // No existe la enfermera
+          var_dump('ve a mamar a otra persona');
+          $result = null;
+          $status = false;
+          $msg = "Enfermera no autorizada";
+        }
+      }else{ // No existe peticion
+        $result = null;
+        $status = false;
+        $msg = "No existe una petición";
+      }
+
+      // Respuesta
+      return $this->response->send(
+        $result,
+        $status,
+        $msg,
+        []
+      );
+
+    }
+
     public function emergencyNow(){
       $where = array(
         "enabled"=> new FluentLiteral("1")
